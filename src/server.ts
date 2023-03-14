68d085813fa06e00f3d130d5f11b2868ed8c39fb
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-03-14 14:29:29 -0300

    Criação de um usuário

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then(() => {
-  console.log(
-    "🚀🚀🚀 HTTP server running, listening on port " + env.PORT + " 🚀🚀🚀"
-  );
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(
+      '🚀🚀🚀 HTTP server running, listening on port ' + env.PORT + ' 🚀🚀🚀',
+    )
+  })
