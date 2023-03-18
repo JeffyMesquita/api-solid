61bd08ead43c2683efa1b507ac99ccbc572d1615
Jeferson Mesquita <je_2742@hotmail.com>
2023-03-18 11:05:55 -0300

    Caso de uso de validar de check-in

--- src/repositories/inMemory/in-memory-check-ins-repository.ts
+++ src/repositories/inMemory/in-memory-check-ins-repository.ts
@@ -1,10 +1,10 @@
-import { CheckIn, Prisma } from '@prisma/client'
-import dayjs from 'dayjs'
-import { randomUUID } from 'node:crypto'
-import { CheckInsRepository } from '../check-ins-repository'
+import { CheckIn, Prisma } from "@prisma/client";
+import dayjs from "dayjs";
+import { randomUUID } from "node:crypto";
+import { CheckInsRepository } from "../check-ins-repository";
 
 class InMemoryCheckInsRepository implements CheckInsRepository {
-  public items: CheckIn[] = []
+  public items: CheckIn[] = [];
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
@@ -13,41 +13,61 @@ class InMemoryCheckInsRepository implements CheckInsRepository {
       gym_id: data.gym_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
       created_at: new Date(),
-    }
+    };
 
-    this.items.push(checkIn)
+    this.items.push(checkIn);
 
-    return checkIn
+    return checkIn;
   }
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const startOfTheDay = dayjs(date).startOf('date')
-    const endOfTheDay = dayjs(date).endOf('date')
+    const startOfTheDay = dayjs(date).startOf("date");
+    const endOfTheDay = dayjs(date).endOf("date");
 
     const checkInOnSameDate = this.items.find((checkIn) => {
-      const checkInDate = dayjs(checkIn.created_at)
+      const checkInDate = dayjs(checkIn.created_at);
       const isOnSameDate =
-        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
 
-      return checkIn.user_id === userId && isOnSameDate
-    })
+      return checkIn.user_id === userId && isOnSameDate;
+    });
 
     if (!checkInOnSameDate) {
-      return null
+      return null;
     }
 
-    return checkInOnSameDate
+    return checkInOnSameDate;
   }
 
   async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
     return this.items
       .filter((item) => item.user_id === userId)
-      .slice((page - 1) * 20, page * 20)
+      .slice((page - 1) * 20, page * 20);
   }
 
   async countByUserId(userId: string): Promise<number> {
-    return this.items.filter((item) => item.user_id === userId).length
+    return this.items.filter((item) => item.user_id === userId).length;
+  }
+
+  async findById(id: string): Promise<CheckIn | null> {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn): Promise<CheckIn> {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
+
+    return checkIn;
   }
 }
 
-export { InMemoryCheckInsRepository }
+export { InMemoryCheckInsRepository };
---
+++ src/use-cases/validate-check-in.spec.ts
@@ -0,0 +1,46 @@
+import { InMemoryCheckInsRepository } from '@/repositories/inMemory/in-memory-check-ins-repository'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { ValidateCheckInUseCase } from './validate-check-in'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInUseCase
+
+describe('Validate Check-in Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new ValidateCheckInUseCase(checkInsRepository)
+
+    // vi.useFakeTimers();
+  })
+
+  afterEach(() => {
+    // vi.useRealTimers();
+  })
+
+  const latitude = -20.9398483
+  const longitude = -48.4887321
+
+  it('should be able to validate check-in', async () => {
+    // vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('should be able to validate check-in', async () => {
+    await expect(() =>
+      sut.execute({
+        checkInId: 'inexistent-check=in-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
