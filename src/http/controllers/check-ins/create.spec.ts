38bceb28f3b6ead33250f4f6d4f3424b740d200e
Jeferson Mesquita <je_2742@hotmail.com>
2023-03-22 20:35:06 -0300

    Testes E2E de rotas de check-ins

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,39 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '17999999999',
+        latitude: -20.9398483,
+        longitude: -48.4887321,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -20.9398483,
+        longitude: -48.4887321,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
